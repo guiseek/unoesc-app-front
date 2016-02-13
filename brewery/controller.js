0e269a1bbee482c4f0b4d5408fd1e13b87552dbf
Guilherme Siquinelli <guiseek@gmail.com>
2016-02-13 12:43:33 -0200

    Brewery findAll and save

--- brewery/controller.js
+++ brewery/controller.js
@@ -9,5 +9,41 @@
     
     function BreweryController(BreweryService) {
         var vm = this;
+        vm.empty = {};
+        
+        vm.findAll = function() {
+            BreweryService.findAll().then(function(response) {
+                console.log(response);
+                vm.breweries = response.data;
+            },function(error) {
+                console.error(error);
+            });
+        }
+        vm.findAll();
+        
+        vm.reset = function() {
+            vm.brewery = angular.copy(vm.empty);
+        }
+        vm.save = function(brewery) {
+            if (brewery._id) {
+                BreweryService.update(brewery).then(function(response) {
+                    vm.success = response.data;
+                    vm.findAll();
+                    vm.reset();
+                },function(error) {
+                    console.log(error);
+                    vm.error = error.data;
+                });
+            } else {
+                BreweryService.create(brewery).then(function(response) {
+                    vm.success = response.data;
+                    vm.findAll();
+                    vm.reset();
+                }, function(error) {
+                    console.error(error);
+                    vm.error = error.data;
+                });
+            }
+        }
     }
 })();
